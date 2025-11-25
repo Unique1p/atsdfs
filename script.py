e6803d43a4207c62e23c5d01d951de63592c68b8
Unique1p <uniqueps429@gmail.com>
2025-11-25 23:42:57 +0100

    Change print
    Increased the number of characters printed from the response.

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ URL = "https://www.rebogroep.nl/nl/particulier/ons-aanbod/huren"
 
 response = requests.get(URL, timeout=10)
 print("=== HTML BEGIN ===")
-print(response.text[:2000])            # eerste 2000 tekens
+print(response.text[:100000])            # eerste 2000 tekens
 print("=== HTML EINDE ===")
 
 soup = BeautifulSoup(response.text, "html.parser")
